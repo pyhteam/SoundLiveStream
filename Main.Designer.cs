926cb184e43b3643aeee96a472189ed4045620d2
Peb Yog Hmong <87113768+pyhteam@users.noreply.github.com>
2022-01-19 16:39:47 +0700

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SoundLiveStream
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbClose = new System.Windows.Forms.Label();
+            this.lbMinimux = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.lbMinimux);
+            this.panel1.Controls.Add(this.lbClose);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1019, 33);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbClose
+            // 
+            this.lbClose.AutoSize = true;
+            this.lbClose.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbClose.ForeColor = System.Drawing.Color.Red;
+            this.lbClose.Location = new System.Drawing.Point(992, 0);
+            this.lbClose.Name = "lbClose";
+            this.lbClose.Size = new System.Drawing.Size(24, 25);
+            this.lbClose.TabIndex = 1;
+            this.lbClose.Text = "X";
+            this.lbClose.Click += new System.EventHandler(this.lbClose_Click);
+            // 
+            // lbMinimux
+            // 
+            this.lbMinimux.AutoSize = true;
+            this.lbMinimux.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbMinimux.ForeColor = System.Drawing.Color.Red;
+            this.lbMinimux.Location = new System.Drawing.Point(962, 0);
+            this.lbMinimux.Name = "lbMinimux";
+            this.lbMinimux.Size = new System.Drawing.Size(20, 25);
+            this.lbMinimux.TabIndex = 1;
+            this.lbMinimux.Text = "-";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 659);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sound Live Stream";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbClose;
+        private System.Windows.Forms.Label lbMinimux;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoundLiveStream
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void lbClose_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you want to exit application", "Confirmation", MessageBoxButtons.YesNoCancel);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
